99bb276e832c42238d78cbfee5a9c509a87152fe
watch14 <watchman1414@gmail.com>
2023-12-20 02:05:29 +0000

    fast_push

--- 0x00-challenge/4-delete_dnodeint/lists.h
+++ 0x00-challenge/4-delete_dnodeint/lists.h
@@ -4,14 +4,14 @@
 #include <stddef.h>
 
 /**
- * struct dlistint_s - doubly linked list
- * @n: integer
- * @prev: points to the prev node
- * @next: points to the next node
- *
- * Description: doubly linked list node structure
- * for Holberton project
- */
+* struct dlistint_s - doubly linked list
+* @n: integer
+* @prev: points to the prev node
+* @next: points to the next node
+*
+* Description: doubly linked list node structure
+* for Holberton project
+*/
 typedef struct dlistint_s
 {
 	int n;
